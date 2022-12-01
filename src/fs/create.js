d35f2ff96178ba2c7389ce5409fd6dfaaf9baf59
Victor Leshkov <leshkovvictor@gmail.com>
2022-12-01 21:36:42 +0600

    feat: implement fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
+const fileData = 'I am fresh and young';
+const filePath = path.resolve('./files/fresh.txt'); 
+
 const create = async () => {
     // Write your code here 
+
+    fs.readFile(filePath).then(
+        () => {
+            throw new Error('FS operation failed');
+        },
+        () => {
+            fs.writeFile(filePath, fileData);
+        }
+    );
 };
 
 await create();
