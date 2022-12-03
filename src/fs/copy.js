94c263f31cf28fedfc782de315be14060ec4d681
Victor Leshkov <leshkovvictor@gmail.com>
2022-12-03 20:22:56 +0600

    feat: implement fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import * as fs from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    fs.opendir('./files_copy').then(
+        () => {
+            throw new Error('FS operation failed');
+        },
+        () => {
+            fs.cp('./files', './files_copy', {recursive: true}).then(
+                () => {},
+                () => {
+                    throw new Error('FS operation failed');
+                }
+            ); 
+        }
+    );
 };
 
 copy();
